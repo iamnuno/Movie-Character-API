b9d54195beb5128b7afefcb759ac39ba2bb2131d
Nuno Cunha <33864862+iamnuno@users.noreply.github.com>
2021-02-09 16:42:40 +0100

    feat: adds basic CRUD to characters

--- src/main/java/com/example/moviecharacter/controllers/CharacterController.java
+++ src/main/java/com/example/moviecharacter/controllers/CharacterController.java
@@ -1,5 +1,76 @@
 package com.example.moviecharacter.controllers;
 
-public class CharacterController {
+import com.example.moviecharacter.models.Character;
+import com.example.moviecharacter.repositories.CharacterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1/characters")
+public class CharacterController {
+    @Autowired
+    private CharacterRepository characterRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Character>> getCharacters() {
+        List<Character> characters = characterRepository.findAll();
+        return new ResponseEntity<>(characters, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Character> getCharacterbyId(@PathVariable Long id) {
+        Character returnCharacter = new Character();
+        HttpStatus status;
+
+        if (characterRepository.existsById(id)){
+            status = HttpStatus.OK;
+            returnCharacter = characterRepository.findById(id).get();
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+
+        return new ResponseEntity<>(returnCharacter, status);
+    }
+
+    @PostMapping
+    public ResponseEntity<Character> addCharacter(@RequestBody Character character) {
+        character = characterRepository.save(character);
+        return new ResponseEntity<>(character, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Character> updateCharacter(@PathVariable Long id, @RequestBody Character character) {
+        Character returnCharacter = new Character();
+        HttpStatus status;
+
+        if (!id.equals(character.getId())){
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(returnCharacter, status);
+        }
+
+        returnCharacter = characterRepository.save(character);
+        status = HttpStatus.OK;
+        return new ResponseEntity<>(returnCharacter, status);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Character> updateCharacter(@PathVariable Long id) {
+        Character character = new Character();
+        character.setId(id);
+        HttpStatus status;
+
+        if (characterRepository.existsById(id)){
+            status = HttpStatus.OK;
+            characterRepository.delete(character);
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+
+        return new ResponseEntity<>(null, status);
+    }
 }
--- src/main/java/com/example/moviecharacter/repositories/CharacterRepository.java
+++ src/main/java/com/example/moviecharacter/repositories/CharacterRepository.java
@@ -1,4 +1,9 @@
 package com.example.moviecharacter.repositories;
 
-public interface CharacterRepository {
+import com.example.moviecharacter.models.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
 }
