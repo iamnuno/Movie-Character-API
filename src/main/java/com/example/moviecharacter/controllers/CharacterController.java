88c1e2eced1e118a970395f8a3abbb15d976c275
Nuno Cunha <33864862+iamnuno@users.noreply.github.com>
2021-02-10 12:27:32 +0100

    refactor: move database interaction to character service class

--- src/main/java/com/example/moviecharacter/controllers/CharacterController.java
+++ src/main/java/com/example/moviecharacter/controllers/CharacterController.java
@@ -1,9 +1,8 @@
 package com.example.moviecharacter.controllers;
 
 import com.example.moviecharacter.models.Character;
-import com.example.moviecharacter.repositories.CharacterRepository;
+import com.example.moviecharacter.services.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,79 +12,32 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/characters")
 public class CharacterController {
+
     @Autowired
-    private CharacterRepository characterRepository;
+    private CharacterService characterService;
 
     @GetMapping
     public ResponseEntity<List<Character>> getCharacters() {
-        List<Character> characters = characterRepository.findAll();
-        return new ResponseEntity<>(characters, HttpStatus.OK);
+        return characterService.getCharacters();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Character> getCharacterbyId(@PathVariable Long id) {
-        Character returnCharacter = new Character();
-        HttpStatus status;
-
-        if (characterRepository.existsById(id)){
-            status = HttpStatus.OK;
-            returnCharacter = characterRepository.findById(id).get();
-        } else {
-            status = HttpStatus.NOT_FOUND;
-        }
-
-        return new ResponseEntity<>(returnCharacter, status);
+    public ResponseEntity<Character> getCharacterById(@PathVariable Long id) {
+        return characterService.getCharacterById(id);
     }
 
     @PostMapping
     public ResponseEntity<Character> addCharacter(@RequestBody Character character) {
-        character = characterRepository.save(character);
-        return new ResponseEntity<>(character, HttpStatus.CREATED);
+        return characterService.addCharacter(character);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Character> updateCharacter(@PathVariable Long id, @RequestBody Character characterUpdate) {
-        if (characterRepository.existsById(id)) {
-            if (id == characterUpdate.getId()) {
-                Character character = characterRepository.findById(id).get();
-
-                if (characterUpdate.getFullName() != null)
-                    character.setFullName(characterUpdate.getFullName());
-
-                if (characterUpdate.getAlias() != null)
-                    character.setAlias(characterUpdate.getAlias());
-
-                if (characterUpdate.getGender() != null)
-                    character.setGender(characterUpdate.getGender());
-
-                if (characterUpdate.getPicture() != null)
-                    character.setPicture(characterUpdate.getPicture());
-
-                if (characterUpdate.getMovies() != null) {
-                    character.setMovies(characterUpdate.getMovies());
-                }
-
-                characterRepository.save(character);
-                return new ResponseEntity<>(character, HttpStatus.OK);
-            }
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        return characterService.updateCharacter(id, characterUpdate);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Character> updateCharacter(@PathVariable Long id) {
-        Character character = new Character();
-        character.setId(id);
-        HttpStatus status;
-
-        if (characterRepository.existsById(id)){
-            status = HttpStatus.OK;
-            characterRepository.delete(character);
-        } else {
-            status = HttpStatus.NOT_FOUND;
-        }
-
-        return new ResponseEntity<>(null, status);
+    public ResponseEntity<Character> deleteCharacter(@PathVariable Long id) {
+        return characterService.deleteCharacter(id);
     }
 }
