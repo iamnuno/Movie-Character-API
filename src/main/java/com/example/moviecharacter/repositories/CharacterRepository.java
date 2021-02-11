84fa242a931b4ed2503c4ed8b579613e292ba010
Nuno Cunha <33864862+iamnuno@users.noreply.github.com>
2021-02-11 09:19:54 +0100

    refactor: remove unused imports

--- src/main/java/com/example/moviecharacter/repositories/CharacterRepository.java
+++ src/main/java/com/example/moviecharacter/repositories/CharacterRepository.java
@@ -2,11 +2,7 @@ package com.example.moviecharacter.repositories;
 
 import com.example.moviecharacter.models.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Long> {
