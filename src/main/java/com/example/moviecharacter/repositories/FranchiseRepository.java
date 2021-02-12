3ede54fdc7ac9fc96b68986d334ceb8c3a10e55f
Maskinexpert <maskinexpert@gmail.com>
2021-02-12 11:43:48 +0100

    doc: added javadoc comments to FranchiseRepository

--- src/main/java/com/example/moviecharacter/repositories/FranchiseRepository.java
+++ src/main/java/com/example/moviecharacter/repositories/FranchiseRepository.java
@@ -4,6 +4,9 @@ import com.example.moviecharacter.models.Franchise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * FranchiseRepository interface makes it possible to implement JpaRepository methods.
+ */
 @Repository
 public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
 }
